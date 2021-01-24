b9e5d32c30dc6d602e440f9b8a41789be7341872
krishkumar3 <64547403+krishkumar3@users.noreply.github.com>
2021-01-24 17:56:13 +0530

    Update BANKNIFTY.py

--- BANKNIFTY.py
+++ BANKNIFTY.py
@@ -9,22 +9,33 @@ from nsetools import Nse
 from datetime import date
 from datetime import timedelta
 import datetime
+import time
 
 
-nse = Nse()
-cmp = nse.get_index_quote('NIFTY BANK')
-cmp = cmp['lastPrice']
-cmp = int(round(cmp/100.0)*100.0)
+
+def basic():
+    nse = Nse()
+    cmp = nse.get_index_quote('NIFTY BANK')
+    cmp = cmp['lastPrice']
+    cmp = int(round(cmp/50.0)*50.0)
+    l=[]
+
+    new_url = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        page = requests.get(new_url,headers=headers)    
+        dajs = page.json()
+        l.append(dajs)
+        l.append(cmp)
+    except:
+        page = requests.get(new_url,headers=headers)    
+        dajs = page.json()
+        l.append(dajs)
+        l.append(cmp)
+    return l
 
 
-new_url = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
-
-headers = {'User-Agent': 'Mozilla/5.0'}
-page = requests.get(new_url,headers=headers)
-dajs = page.json()
-
-
-def plot_openInterest(c,p):
+def plot_openInterest(c,p,cmp):
 
     ax = c.plot(figsize=(15,6),x='strikePrice',y='openInterest',title=cmp,kind='bar',color='red',xlabel='Strike Price',ylabel='Price'
             ,yticks=list(range(0,max(c['openInterest']+p['openInterest']),12000)),width=0.85)
@@ -35,7 +46,7 @@ def plot_openInterest(c,p):
     p.plot(figsize=(15,6),ax=ax,x='strikePrice',y='changeinOpenInterest',kind='bar',color='black',yticks=list(range(0,max(c['openInterest']+p['openInterest']),12000)),width=0.45)
     ax.legend(["Open Interest CE", "Open Interest PE","Change in Open Interest CE","Change in Open Interest PE"])
 
-def plot_prices(ce_dt,pe_dt):
+def plot_prices(ce_dt,pe_dt,cmp):
     ce = ce_dt[['lastPrice','strikePrice','openInterest']]
     pe = pe_dt[['lastPrice','strikePrice','openInterest']]
     r = ce_dt['strikePrice']
@@ -57,7 +68,7 @@ def plot_prices(ce_dt,pe_dt):
     
 
     
-def fetch_oi(expiry_dt):
+def fetch_oi(expiry_dt,dajs,cmp):
     ce_values = [data['CE'] for data in dajs['records']['data'] if "CE" in data and data['expiryDate'] == expiry_dt]
     pe_values = [data['PE'] for data in dajs['records']['data'] if "PE" in data and data['expiryDate'] == expiry_dt]
 
@@ -71,12 +82,13 @@ def fetch_oi(expiry_dt):
     c=ce[50:]
     p=pe[50:]
 
-    plot_openInterest(c,p)
+    plot_openInterest(c,p,cmp)
     #plot_prices(ce_dt,pe_dt)
     plt.subplots_adjust(bottom=0.13,left=0.065,right=0.97, top=0.96)
-    plt.show()
+    plt.show(block=False)
+    plt.pause(60)
+    plt.close()
 
-  
 def main():
     today = date.today()
     offset = (today.weekday()) 
@@ -85,6 +97,11 @@ def main():
     else:
         thursday = today + timedelta(days=10-offset)
     expiry_dt = str(thursday.strftime('%d-%b-%Y'))
-    fetch_oi(expiry_dt)
+    t=time.time()
+    while time.time() < t+900:
+        dajs = basic()
+        cmp = dajs[1]
+        dajs = dajs[0]
+        fetch_oi(expiry_dt,dajs,cmp)
 if __name__ == '__main__':
     main()
